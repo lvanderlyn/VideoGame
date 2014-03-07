c27caa75a0520add82a752577b09581d88ba9092
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-03-06 22:58:17 -0500

    work in progress on a better classification system for contact between actor and various background objects, should resolve a lot of the current bugs we have been experiencing

--- isContact_L1.py
+++ isContact_L1.py
@@ -18,8 +18,8 @@ mainClock = pygame.time.Clock()
 FPS = 30
 
 # set up the window
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
+WINDOWWIDTH = 1600
+WINDOWHEIGHT = 850
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Ladders')
 
@@ -90,79 +90,120 @@ class Jumpman(object):
         
 
 class Platform(Jumpman):
-    def __init__(self, position, width, height):
+    #The platform class has the below attributes
+    def __init__(self, position, width, height, identity):
         self.position = position
         self.velocity = (0,0)
         self.width = width
         self.height = height
+        self.identity = identity
 
 
 
 player = Jumpman((0,0),(0,0), MAN_WIDTH, MAN_HEIGHT)
 pygame.draw.rect(windowSurface, WHITE, (player.getX(), player.getY(), player.getWidth(),player.getHeight()))
 
-ladder = Platform((200,200),LADDER_WIDTH,LADDER_HEIGHT)
+ladder = Platform((200,200),LADDER_WIDTH,LADDER_HEIGHT, 'ladder')
 pygame.draw.rect(windowSurface, GREEN, (ladder.getX(), ladder.getY(), ladder.getWidth(),ladder.getHeight()))
 
-plat = Platform((0,300),PLATFORM_WIDTH,PLATFORM_HEIGHT)
-pygame.draw.rect(windowSurface, BLUE, (plat.getX(), plat.getY(), plat.getWidth(),plat.getHeight()))
+plat1 = Platform((0,300),PLATFORM_WIDTH,PLATFORM_HEIGHT, 'platform')
+pygame.draw.rect(windowSurface, BLUE, (plat1.getX(), plat1.getY(), plat1.getWidth(),plat1.getHeight()))
 
+plat2 = Platform((0,200), PLATFORM_WIDTH, PLATFORM_HEIGHT, 'platform')
+pygame.draw.rect(windowSurface, BLUE, (plat2.getX(), plat2.getY(), plat2.getWidth(), plat2.getHeight()))
 
+background = [plat1, plat2, ladder]
 MODE_CLIMB = 1
 MODE_WALK = 2
 MODE_FALL = 3
 MODE_CANCLIMB = 4
 
 
-    
-#
-#
+
+
 #def isContact(actor, other):
-#    '''takes in the jumpman actor and an object and determines if there is contact between the two'''
-#    actor_rect = pygame.Rect(actor.getX(),actor.getY(),actor.getWidth()+1,actor.getHeight()+1)
-#    other_rect = pygame.Rect(actor.getX(),actor.getY(),other.getWidth()+1,other.getHeight()+1)
-#    contact = other_rect.colliderect(actor_rect)
-#    if contact:
-#        print "True"
+#    '''hopefull actually tells if there is contact between the bottom of the
+#    actor and some other object'''
+#    actor_rect = pygame.Rect(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight())
+#    other_rect = pygame.Rect(other.getX(), other.getY(), other.getWidth(), other.getHeight())
+#    if (actor_rect.right - 0.75* actor_rect.width <= other_rect.right) and (actor_rect.right - 0.25*actor_rect.width >= other_rect.left):
+#        if actor_rect.bottom >= other_rect.top:
+#            return True
 #    else:
-#        print "False"
-#    return contact
-
-def isContact(actor, other):
-    '''hopefull actually tells if there is contact between the bottom of the
-    actor and some other object'''
-    actor_rect = pygame.Rect(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight())
-    other_rect = pygame.Rect(other.getX(), other.getY(), other.getWidth(), other.getHeight())
-    if (actor_rect.right - 0.75* actor_rect.width <= other_rect.right) and (actor_rect.right - 0.25*actor_rect.width >= other_rect.left):
-        if actor_rect.bottom >= other_rect.top:
-            return True
-    else:
-        return False
+#        return False
+           
 
 
-def modeSelect(actor, climb, walk):
-    '''take in a list of climable object called 'climb', and list of platforms
-    called 'walk' and determines the mode (controls allowed) based on what
-    the jumperman is in contact with'''
-    mode = 0
-    Climb = False
-    Walk = False
-    for up in climb:
-        if isContact(actor, up):
-            Climb = True
-            break
-    for platform in walk:
-        if isContact(actor, platform):
-            Walk = True
-            break
-    if Walk and Climb:
-        return MODE_CANCLIMB
-    elif Walk and (not Climb):
-        return MODE_WALK
-    elif Climb and (not Walk):
-        return MODE_CLIMB
-    elif not Climb and (not Walk):
-        return MODE_FALL
+#def modeSelect(actor, climb, walk):
+#    '''take in a list of climable object called 'climb', and list of platforms
+#    called 'walk' and determines the mode (controls allowed) based on what
+#    the jumperman is in contact with'''
+#    
+#    mode = 0
+#    Climb = False
+#    Walk = False
+#    for up in climb:
+#        #checks through objects that are ladders
+#        if isContact(actor, up):
+#            Climb = True
+#            break
+#    for platform in walk:
+#        #checksthrough objects that are platforms
+#        if isContact(actor, platform):
+#            Walk = True
+#            break
+#    if Walk and Climb:
+#        return MODE_CANCLIMB
+#    elif Walk and (not Climb):
+#        return MODE_WALK
+#    elif Climb and (not Walk):
+#        return MODE_CLIMB
+#    elif not Climb and (not Walk):
+#        return MODE_FALL
+
+
+def modeSelect(actor, background):
+    #Let's think about things logically:
+    #We have a problem -> ie we want to be able to determine the mode that we
+    #are based on the things that we are contacting
+    #Basically, there are several modes that we need to consider:
+        #1) under a ladder
+            #- actor would have contact with the ladder at the top bottom and sides and platform 
+            #- needs to be able to climb up or walk side to side
+        #2) above a ladder
+            #- actor would have contact only at the bottom and with the platform
+            #- needs to be able to climb down or walk side to side
+        #3) on a ladder
+            #- actor would have contact with ladder, but not platform
+            #- needs to be able to move up and down, but not sideways
+        #4 on ground
+            #- actor would have contact with platform and not ladder
+            #-actor would need to move side to side but not up or down
+        #5 falling
+            #-actor is in contact with now objects
+            #- can move side to side, and fall due to gravity
+            
+    for piece in background:
+        #runs through all the background objects and decides the contact conditions
+        #based on how the objects are interacting and they type of object
+        betweenLadder = False
+        withinLadder = False
+        aboveLadder = False
+        onPlatform = False
+        if piece.type == 'ladder':
+            if actor.getX() + 0.75 * actor.getWidth() >= piece.getX() and actor.getX() + 0.25*actor.getWidth() <= piece.getX() + piece.getWidth():
+                betweenLadder = True
+            if actor.getY() - actor.getHeight() >= piece.getY() - piece.getHeight() and actor.getY() - actor.getHeight() < piece.getY():
+                withinLadder = True
+            if actor.getY() - actor.getHeight() == piece.getY():
+                aboveLadder = True
+        if piece.type == 'platform':
+            if actor.getY() - actor.getHeight() == piece.getY():
+                if actor.getX() + 0.75 * actor.Width() >= piece.getX() and actor.getX() + 0.25*actor.getWidth() <= piece.getX() + piece.getWidth():
+                    onPlatform = True
+    #Assign Modes here based on the above conditions
+    
+    
         
 def gravity(jumpman, dt):
     """
@@ -178,7 +219,7 @@ def gravity(jumpman, dt):
 
 
 while True:
-    mode = modeSelect(player, [ladder], [plat])    
+    mode = modeSelect(player, [ladder], plat)    
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
@@ -198,7 +239,7 @@ while True:
             if event.key == K_DOWN or event.key == ord('s'):
                 moveUp = False
                 moveDown = True
-        if event.type == KEYUP:                     #denotes that no keys are pushed down
+        if event.type == KEYUP:                     #denotes that no keys are pushed down (ie and reset)
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
@@ -219,8 +260,6 @@ while True:
     # move the player
     player.setVelocity((0,0))
     if mode == 4:
-        if moveDown and player.getY()+player.getHeight() < WINDOWHEIGHT:
-            player.setVelocity((0,0))
         if moveUp and player.getY() > 0:
             player.setVelocity((0,-1*MOVESPEED))
         if moveLeft and player.getX() > 0:
@@ -228,10 +267,6 @@ while True:
         if moveRight and player.getX()+player.getWidth() < WINDOWWIDTH:
             player.setVelocity((MOVESPEED,0))
     if mode == 2:
-        if moveDown and player.getY()+player.getHeight() < WINDOWHEIGHT:
-            player.setVelocity((0,0))
-        if moveUp and player.getY() > 0:
-            player.setVelocity((0,0))
         if moveLeft and player.getX() > 0:
             player.setVelocity((-1*MOVESPEED,0))
         if moveRight and player.getX()+player.getWidth() < WINDOWWIDTH:
@@ -241,15 +276,7 @@ while True:
             player.setVelocity((0,MOVESPEED))
         if moveUp and player.getY() > 0:
             player.setVelocity((0,-1*MOVESPEED))
-        if moveLeft and player.getX() > 0:
-            player.setVelocity((0,0))
-        if moveRight and player.getX()+player.getWidth() < WINDOWWIDTH:
-            player.setVelocity((0,0))
     if mode == 3:
-        if moveDown and player.getY()+player.getHeight() < WINDOWHEIGHT:
-            player.setVelocity((0,0))
-        if moveUp and player.getY() > 0:
-            player.setVelocity((0,0))
         if moveLeft and player.getX() > 0:
             player.setVelocity((-1*MOVESPEED,0))
         if moveRight and player.getX()+player.getWidth() < WINDOWWIDTH:
@@ -258,7 +285,8 @@ while True:
 
     # draw the player onto the surface
     pygame.draw.rect(windowSurface, GREEN, (ladder.getX(), ladder.getY(), ladder.getWidth(),ladder.getHeight()))
-    pygame.draw.rect(windowSurface, BLUE, (plat.getX(), plat.getY(), plat.getWidth(),plat.getHeight()))
+    pygame.draw.rect(windowSurface, BLUE, (plat1.getX(), plat1.getY(), plat1.getWidth(),plat1.getHeight()))
+    pygame.draw.rect(windowSurface, BLUE, (plat2.getX(), plat2.getY(), plat1.getWidth(),plat2.getHeight()))    
     player.updatePosition(dt)
     pygame.draw.rect(windowSurface, WHITE, (player.getX(), player.getY(), MAN_WIDTH,MAN_HEIGHT))
     
